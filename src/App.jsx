8972fc63a97d88719380268af3cfe03e4b10697c
Salimxadjayeva Komila <salimxadjayevakomila@Salimxadjayevas-MacBook-Air.local>
2026-03-01 19:39:00 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
  import React from 'react'
+import { StickyNavbar } from './components/Navbar'
  
  const App = () => {
    return (
-     <div></div>
+     <div>
+   <StickyNavbar/>
+      </div>
    )
  }
  
