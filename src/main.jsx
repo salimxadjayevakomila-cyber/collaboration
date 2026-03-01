d4f74a2be8133f300dc3d0cf7e26f8a054b45fd9
Salimxadjayeva Komila <salimxadjayevakomila@Salimxadjayevas-MacBook-Air.local>
2026-03-01 22:27:38 +0500

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,12 @@ import App from "./App";
 import "./index.css";
 
 import { ThemeProvider } from "@material-tailwind/react";
-import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 );
